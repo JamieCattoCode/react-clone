8bcabe9928f1c8949bcf0c87b8f72357983b175c
JamieCattoCode <jamietcatto@gmail.com>
2023-06-07 07:07:03 +0200

    Added basic component rendering

---
+++ index.js
@@ -0,0 +1,15 @@
+import { render } from "./MyReact.js";
+import Component from "./Component.js";
+
+let propCount = 0;
+
+document.getElementById('btn-prop').addEventListener('click', () => {
+    propCount++;
+    renderComponent();
+})
+
+function renderComponent() {
+    render(Component, { propCount }, document.getElementById('root'));
+};
+
+renderComponent();
