d1d1d4afbe53c6700bd1ff86499e344748dcc81b
JamieCattoCode <jamietcatto@gmail.com>
2023-06-08 07:04:10 +0200

    Added hooks

--- Component.js
+++ Component.js
@@ -1,6 +1,18 @@
-export default function Component({ propCount }) {
-    const count = 0;
-    const propCountDoubled = 0;
+import { useState, useEffect, useMemo } from "./MyReact.js";
+
+export default function Component({ propCount, buttonElement }) {
+    const [count, setCount] = useState(1);
+    const propCountDoubled = useMemo(() => {
+        return propCount * 2
+    }, [propCount]);
+
+    useEffect(() => {
+      const handler = () => setCount(currentCount => currentCount + 1);
+      buttonElement.addEventListener('click', handler);
+    
+      return () => buttonElement.removeEventListener('click', handler);
+    }, [buttonElement])
+    
 
     return `
         State: ${count}
--- MyReact.js
+++ MyReact.js
@@ -1,4 +1,120 @@
-export function render(component, props, parent) {
-    const output = component(props);
-    parent.textContent = output;
+let globalId = 0;
+let globalParent;
+let componentState = new Map();
+
+export function useState(initialState) {
+    const id = globalId;
+    const parent = globalParent;
+
+    globalId++;
+
+    return (() => {
+        const { cache } = componentState.get(parent);
+    
+        if(cache[id] == null) {
+            // This case denotes that it's the first call of useState for this component
+            cache[id] = { 
+                value: 
+                typeof initialState === 'function' ? initialState() : initialState, 
+            }
+        }
+    
+        const setState = (state) => {
+            const { component, props } = componentState.get(parent)
+            if (typeof state === 'function') {
+                cache[id].value = state(cache[id].value)
+            } else {
+                cache[id].value = state;
+            }
+            // Need to re-render to affect the changes
+            render(component, props, parent);
+        }
+    
+        return [cache[id].value, setState];
+    })()
+
+};
+
+export function useEffect(callback, dependencies) {
+    const id = globalId;
+    const parent = globalParent;
+
+    globalId++;
+
+    (() => {
+        const { cache } = componentState.get(parent);
+    
+        if(cache[id] == null) {
+            cache[id] = { dependencies: undefined }
+        }
+    
+        // Dependencies being null indicates that they're empty or the first iteration
+        // In react, useEffect is always run on the first render, or every re-render if the dependencies are empty
+        // So in either of these cases, we want dependenciesChanged to be true so that we run the callback
+
+        const dependenciesChanged = dependencies == null || 
+        dependencies.some((dependency, i) => {
+                return (
+                    cache[id].dependencies == null || 
+                    cache[id].dependencies[i] !== dependency
+                )
+            }
+        )
+
+        if (dependenciesChanged) {
+            if(cache[id].cleanup != null) cache[id].cleanup()
+            cache[id].cleanup = callback();
+            cache[id].dependencies = dependencies;
+        }
+
+    })()
+};
+
+export function useMemo(callback, dependencies) {
+    const id = globalId;
+    const parent = globalParent;
+
+    globalId++;
+
+    return (() => {
+        const { cache } = componentState.get(parent);
+    
+        if(cache[id] == null) {
+            cache[id] = { dependencies: undefined }
+        }
+    
+        // Dependencies being null indicates that they're empty or the first iteration
+        // In react, useEffect is always run on the first render, or every re-render if the dependencies are empty
+        // So in either of these cases, we want dependenciesChanged to be true so that we run the callback
+
+        const dependenciesChanged = dependencies == null || 
+        dependencies.some((dependency, i) => {
+                return (
+                    cache[id].dependencies == null || 
+                    cache[id].dependencies[i] !== dependency
+                )
+            }
+        )
+
+        if (dependenciesChanged) {
+            cache[id].value = callback();
+            cache[id].dependencies = dependencies;
+        }
+
+        return cache[id].value
+
+    })()
 }
+
+export function render(component, props, parent) {
+    const state = componentState.get(parent) || { cache: [] };
+    componentState.set(parent, {...state, component, props});
+    globalParent = parent;
+    
+    const output = component(props);
+
+    // Set globalId to 0 after we've finished rendering the component
+    globalId = 0;
+
+    parent.textContent = output;
+};
